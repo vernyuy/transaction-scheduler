240927119d9a71d0356656d3dd1ef983d304a7a6
fonchu <hintekk@fonchu.local>
2024-09-13 22:23:45 +0100

    latest updates

--- index.ts
+++ index.ts
@@ -1,8 +1,27 @@
 import { startScheduler } from './tasks/schedular';
 import dotenv from 'dotenv';
 
+import express, { Request, Response, Application } from 'express';
+import bodyParser from "body-parser";
+import cors from "cors";
+
 // Load environment variables from .env file
 dotenv.config();
 
 // Start the scheduler
 startScheduler();
+
+const app = express();
+
+
+app.use(bodyParser.json());
+const origin = {
+  origin: "*",
+};
+app.use(cors(origin));
+const port = 8282;
+
+app.use('/api', ()=>{});
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
